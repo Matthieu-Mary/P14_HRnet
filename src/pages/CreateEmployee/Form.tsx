a2e84761df9cc1c78da85da61bd92a673427781c
Matthieu Mary <matthieu.mary2@gmail.com>
2023-04-03 11:25:16 +0200

    improve css of the form

--- src/pages/CreateEmployee/Form.tsx
+++ src/pages/CreateEmployee/Form.tsx
@@ -1,20 +1,36 @@
 type Props = {};
 
 const Form = (props: Props) => {
+
+  const handleSubmit = () => {
+    console.log("c'est submit")
+  }
+
   return (
     <div className="form-container">
-      <form action="#" id="create-employee">
-        <label htmlFor="first-name">First Name</label>
-        <input type="text" id="first-name" />
+      <form action="#" id="create-employee" onSubmit={handleSubmit}>
+        <div className="names-container">
+          <div className="first-name">
+            <label htmlFor="first-name">First Name</label>
+            <input type="text" id="first-name" />
+          </div>
 
-        <label htmlFor="last-name">Last Name</label>
-        <input type="text" id="last-name" />
+          <div className="last-name">
+            <label htmlFor="last-name">Last Name</label>
+            <input type="text" id="last-name" />
+          </div>
+        </div>
 
-        <label htmlFor="date-of-birth">Date of Birth</label>
-        <input id="date-of-birth" type="text" />
-
-        <label htmlFor="start-date">Start Date</label>
-        <input id="start-date" type="text" />
+        <div className="dates-container">
+          <div className="birth-date">
+            <label htmlFor="date-of-birth">Date of Birth</label>
+            <input id="date-of-birth" type="text" />
+          </div>
+          <div className="start-date">
+            <label htmlFor="start-date">Start Date</label>
+            <input id="start-date" type="text" />
+          </div>
+        </div>
 
         <fieldset className="address">
           <legend>Address</legend>
@@ -31,18 +47,18 @@ const Form = (props: Props) => {
           <label htmlFor="zip-code">Zip Code</label>
           <input id="zip-code" type="number" />
         </fieldset>
-
-        <label htmlFor="department">Department</label>
-        <select name="department" id="department">
-          <option>Sales</option>
-          <option>Marketing</option>
-          <option>Engineering</option>
-          <option>Human Resources</option>
-          <option>Legal</option>
-        </select>
+        <div className="departments">
+          <label htmlFor="department">Department</label>
+          <select name="department" id="department">
+            <option>Sales</option>
+            <option>Marketing</option>
+            <option>Engineering</option>
+            <option>Human Resources</option>
+            <option>Legal</option>
+          </select>
+        </div>
+        <button>Save</button>
       </form>
-
-      <button>Save</button>
     </div>
   );
 };
