1b48609bb63e8144bfc28bd29df51450986c3028
Matthieu Mary <matthieu.mary2@gmail.com>
2023-04-03 14:49:38 +0200

    add name on form's inputs

--- src/pages/CreateEmployee/Form.tsx
+++ src/pages/CreateEmployee/Form.tsx
@@ -26,9 +26,11 @@ const Form = () => {
     department: "",
   });
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
     const { name, value } = event.target;
-    setEmployee(prev => ({
+    setEmployee((prev) => ({
       ...prev,
       [name]: value,
     }));
@@ -45,23 +47,47 @@ const Form = () => {
         <div className="names-container">
           <div className="first-name">
             <label htmlFor="first-name">First Name</label>
-            <input type="text" id="first-name" value={employee.firstName} onChange={(e) => handleChange(e)}/>
+            <input
+              type="text"
+              id="first-name"
+              name="firstName"
+              value={employee.firstName}
+              onChange={(e) => handleChange(e)}
+            />
           </div>
 
           <div className="last-name">
             <label htmlFor="last-name">Last Name</label>
-            <input type="text" id="last-name" value={employee.lastName} onChange={(e) => handleChange(e)}/>
+            <input
+              type="text"
+              id="last-name"
+              name="lastName"
+              value={employee.lastName}
+              onChange={(e) => handleChange(e)}
+            />
           </div>
         </div>
 
         <div className="dates-container">
           <div className="birth-date">
             <label htmlFor="date-of-birth">Date of Birth</label>
-            <input id="date-of-birth" type="text" value={employee.dateOfBirth} onChange={(e) => handleChange(e)} />
+            <input
+              id="date-of-birth"
+              type="text"
+              name="dateOfBirth"
+              value={employee.dateOfBirth}
+              onChange={(e) => handleChange(e)}
+            />
           </div>
           <div className="start-date">
             <label htmlFor="start-date">Start Date</label>
-            <input id="start-date" type="text" value={employee.startDate} onChange={(e) => handleChange(e)} />
+            <input
+              id="start-date"
+              type="text"
+              name="startDate"
+              value={employee.startDate}
+              onChange={(e) => handleChange(e)}
+            />
           </div>
         </div>
 
@@ -69,20 +95,54 @@ const Form = () => {
           <legend>Address</legend>
 
           <label htmlFor="street">Street</label>
-          <input id="street" type="text" value={employee.street}  onChange={(e) => handleChange(e)}/>
+          <input
+            id="street"
+            type="text"
+            name="street"
+            value={employee.street}
+            onChange={(e) => handleChange(e)}
+          />
 
           <label htmlFor="city">City</label>
-          <input id="city" type="text" value={employee.city} onChange={(e) => handleChange(e)} />
+          <input
+            id="city"
+            type="text"
+            name="city"
+            value={employee.city}
+            onChange={(e) => handleChange(e)}
+          />
 
           <label htmlFor="state">State</label>
-          <select name="state" id="state" value={employee.state} onChange={(e) => handleChange(e)}></select>
+          <select
+            name="state"
+            id="state"
+            value={employee.state}
+            onChange={(e) => handleChange(e)}
+          >
+            {states.map((state) => (
+              <option value={state.abbreviation} key={state.abbreviation}>
+                {state.name}
+              </option>
+            ))}
+          </select>
 
           <label htmlFor="zip-code">Zip Code</label>
-          <input id="zip-code" type="number" value={employee.zipCode} onChange={(e) => handleChange(e)} />
+          <input
+            id="zip-code"
+            type="number"
+            name="zipCode"
+            value={employee.zipCode}
+            onChange={(e) => handleChange(e)}
+          />
         </fieldset>
         <div className="departments">
           <label htmlFor="department">Department</label>
-          <select name="department" id="department" value={employee.department} onChange={(e) => handleChange(e)}>
+          <select
+            name="department"
+            id="department"
+            value={employee.department}
+            onChange={(e) => handleChange(e)}
+          >
             <option>Sales</option>
             <option>Marketing</option>
             <option>Engineering</option>
